27cff024a9ffab3dfaf8dd3e88b00eaabf501fc9
ZimouSun <zimousun@bu.edu>
2022-08-23 22:10:12 -0400

    1

--- Homework1/src/main/java/org/example/Main.java
+++ Homework1/src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /**
  * Zimou Sun
  * Tue Aug 23 2022
@@ -20,10 +24,41 @@ public class Main {
     {
         return str1.equals(str2) && str1.compareTo(str2)==0;
     }
+    public static List<String> String_Problem_3()
+    {
+        String input ="https://www.amazon.com/demo?test=abc";
+        String regex = "[:/+.?=]";
+        StringTokenizer st= new StringTokenizer(input,regex);
+        List<String> tokens = new ArrayList<String>();
+
+        while(st.hasMoreTokens())
+        {
+            tokens.add(st.nextToken());
+        }
+
+        return tokens;
+    }
+
+    public static StringBuilder String_Problem_4( List<String[]> listOfStringArray)
+    {
+        StringBuilder s= new StringBuilder();
+        for(String[] sa: listOfStringArray)
+        {
+            for(String str : sa)
+            {
+                s.append(str);
+            }
+        }
+    return s;
+    }
 
     public static void main(String[] args) {
 
-        //
+        //Problem 1
+
+        //Problem 2
+
+        //Problem 3
 
 
     }
