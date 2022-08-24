8b19954abafaf312d27781458ddc2d5cb5acec1a
ZimouSun <zimousun@bu.edu>
2022-08-23 21:25:06 -0400

    1

---
+++ Homework1/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+/**
+ * Zimou Sun
+ * Tue Aug 23 2022
+ *
+ * Homework1
+ */
+public class Main {
+
+    public static String[] String_Problem_1 ()
+    {
+        String input="Algorithms";
+
+        return new String[]{input.substring(2, 3), input.substring(0, 3)};
+
+
+    }
+    public static boolean String_Problem_2(String str1, String str2)
+    {
+        return str1.equals(str2) && str1.compareTo(str2)==0;
+    }
+
+    public static void main(String[] args) {
+
+        //
+
+
+    }
+}
