b51c67ecaae6f46a998e440d3356f4aba3cace68
ZimouSun <78500226+ZimouSun@users.noreply.github.com>
2022-09-19 22:16:55 -0400

    done

--- Homework2/src/main/java/DepartmentDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface DepartmentDAO {
-    public List<Department> getAllDepartments();
-    public Department getDepartment( int departmentId);
-    public void addDepartment ( Department d);
-    public void updateDepartment( Department d);
-    public void deleteDepartment( Department d);
-}
