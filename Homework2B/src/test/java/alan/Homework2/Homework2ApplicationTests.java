4c0bb2e30679b4fd3079e57923dcad252bb66290
ZimouSun <78500226+ZimouSun@users.noreply.github.com>
2022-09-19 22:18:16 -0400

    d
    d

---
+++ Homework2B/src/test/java/alan/Homework2/Homework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package alan.Homework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
