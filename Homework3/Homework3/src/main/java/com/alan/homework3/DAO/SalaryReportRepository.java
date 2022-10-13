776a8f805905d42a7fe6ab2bb35ff130cb25ff31
ZimouSun <78500226+ZimouSun@users.noreply.github.com>
2022-10-13 00:00:51 -0400

    done

--- Homework3/Homework3/src/main/java/com/alan/homework3/DAO/SalaryReportRepository.java
+++ Homework3/Homework3/src/main/java/com/alan/homework3/DAO/SalaryReportRepository.java
@@ -3,7 +3,9 @@ package com.alan.homework3.DAO;
 import com.alan.homework3.model.SRID;
 import com.alan.homework3.model.SalaryReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SalaryReportRepository extends JpaRepository<SalaryReport, SRID> {
     public SalaryReport findByJobAndDepartment(String job,String department);
 }
